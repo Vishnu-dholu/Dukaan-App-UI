f208999560ebee65a77a36a82b7c8b34a7b4b5cf
Vishnu-dholu <dholuvishnu10@gmail.com>
2024-04-07 17:30:59 +0530

    Footer Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
             <div>
                 ----- Made By<a className='pl-1 text-blue-700 font-medium' href="https://twitter.com/DholuVishnu">Vishnu Dholu</a>, inspired by<a className='px-1 text-blue-700 font-medium' href="https://twitter.com/subhashchy/status/1744308069751025894?t=MrLV-PSnfsgv0Hg2jTz5JA&s=08">Dukaan App UI</a>-----
             </div>
-            <div  className='text-black-300 underline' ><a href="https://github.com/Shivam29k/dukaaan-app-ui">Get the source code</a></div>
+            <div  className='text-black-300 underline' ><a href="https://github.com/Vishnu-dholu/Dukaan-App-UI">Get the source code</a></div>
         </div>
     )
 }
